8a82b4b864216830cb71763ba409af88d25b6dba
Iva Utikalova <iva.utikalova@dmdox.com>
2023-04-23 19:36:27 +0200

    adding

---
+++ script.js
@@ -0,0 +1,8 @@
+function dropdown_ex() {
+    document.getElementById("ex-button")?.classList.toggle("show");
+}
+
+
+function dropdown_menu() {
+    document.getElementById("menu-button")?.classList.toggle("show");
+}
